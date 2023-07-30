3a120468e950644a8ad5e357ad46582965933b58
SabeloX <sabelomtw@gmail.com>
2023-07-30 23:02:01 +0200

    Removed footer icons for now

--- components/audience/footer.tsx
+++ components/audience/footer.tsx
@@ -3,14 +3,14 @@ export const Footer = () => {
         <div className="container">
             <footer className="footer">       
                 <p className="mb-0">Sabelo &copy; 2023</p>
-                <div className="social-links text-right m-auto ml-sm-auto">
+                {/* <div className="social-links text-right m-auto ml-sm-auto">
                     <a href="javascript:void(0)" className="link"><i className="ti-facebook"></i></a>
                     <a href="javascript:void(0)" className="link"><i className="ti-twitter-alt"></i></a>
                     <a href="javascript:void(0)" className="link"><i className="ti-google"></i></a>
                     <a href="javascript:void(0)" className="link"><i className="ti-pinterest-alt"></i></a>
                     <a href="javascript:void(0)" className="link"><i className="ti-instagram"></i></a>
                     <a href="javascript:void(0)" className="link"><i className="ti-rss"></i></a>
-                </div>
+                </div> */}
             </footer>
         </div>
     )
